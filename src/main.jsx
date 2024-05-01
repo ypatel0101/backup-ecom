72071fc8e253b0cb489d7468350ef2891526d9d5
Tybreen <tyler.breen@icloud.com>
2024-05-01 16:52:54 -0400

    changed to <BrowserRouter>

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<RouterProvider>
+	<BrowserRouter>
 		<App />
-	</RouterProvider>,
+	</BrowserRouter>,
 );
