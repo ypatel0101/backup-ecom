591cf4313128c726a85b24eca0ffd07320287e17
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-05-01 11:37:47 -0700

    removed uunnecessary code

--- server.js
+++ server.js
@@ -2,18 +2,3 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const main = () => {
-	// TODO: write prisma client queries here 
-}
-
-main()
-	.then(async() => {
-		await prisma.$disconnect()
-	})
-	.catch(async (e) => {
-		console.error(e)
-		await prisma.$disconnect
-		process.exit(1)
-	})
-
-export default prisma;
