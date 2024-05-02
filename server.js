e56fa925cefd5bec72159ae4e1fd5fc3a6d41615
Valentino Ramiro <Valentinoramiro777@gmail.com>
2024-05-02 08:44:01 -0700

    initialize express, added middleware to server.js

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-export default prisma;
+import "dotenv/config";
+import express from "express";
+const app = express();
+const log = console.log;
+
+//Body-Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+	log("<___BODY LOGGER START_____>");
+	log(req.body);
+	log("<___BODY LOGGER END_______>");
+	next();
+});
+
+app.listen(process.env.PORT || 3000, () => {
+	log("LISTENING ON PORT", process.env.PORT);
+});
