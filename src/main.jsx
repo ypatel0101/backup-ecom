c7acb736ee8ae4f3e68302704f16c86b6d0db469
Godspeexd12 <160627473+ypatel0101@users.noreply.github.com>
2024-05-02 13:32:06 -0500

    fixed indentations, removed certain things that weren't required for our project

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import { Provider } from "react";
-import { store } from "react";
+import { Provider } from "react-redux";
+import store  from "./store/store.js";
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Provider store ={store}>
+	<Provider store = { store } >
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,9 @@
 //This is the initial Redux Store Setup.
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counter/counterSlice.js';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    
   }
 })
 
